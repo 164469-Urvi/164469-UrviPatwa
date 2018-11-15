cfb0fba08edb57e75604516c65fa460c4524c6e1
164469-Urvi <44639315+164469-Urvi@users.noreply.github.com>
2018-11-15 18:38:36 +0530

    Add files via upload

---
+++ TestBankAccount.java
@@ -0,0 +1,13 @@
+package com.urvi;
+import java.util.* ;
+public class TestBankAccount {
+ public static void testBankAccountMethod()
+ {
+	 BankAccount acc=new BankAccount();
+	 acc.setAccountHolderName("sachin");
+ }
+ public static void main(String[] args) {
+	testBankAccountMethod();
+}
+}
+ 
