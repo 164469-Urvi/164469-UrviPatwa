32c00063f50aa4649357202b6dd58df6c38575b1
164469-Urvi <44639315+164469-Urvi@users.noreply.github.com>
2018-11-20 18:43:27 +0530

    Add files via upload

--- DateDifferenceProvider.java
+++ DateDifferenceProvider.java
@@ -1,64 +1,96 @@
-package DataDifferenceProvider;
+package com.onkar.service;
+
+import com.onkar.service.supp.MyDate;
 
 public class DateDifferenceProvider {
-	static final int JAN=31;
-	static final int FEB=31;
-	static final int MAR=31;
-	static final int APR=31;
-	static final int MAY=31;
-	static final int JUN=31;
-	static final int JUL=31;
-	static final int AUG=31;
-	static final int SEPT=31;
-	static final int OCT=31;
-	static final int NOV=31;
-	static final int DEC=31;
-int [] MONTHS={JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEPT,OCT,NOV,DEC};
-  public int getDateDifferenceProvider(MyDate startdate,MyDate enddate)
-  {
-	if(sameDate(startdate,enddate) && sameMonth(startdate,enddate) && sameYear(startdate,enddate))
-	{	return 0; }
-	else if(sameMonth(startdate,enddate) && sameYear(startdate,enddate))
-	{
-	return enddate.getdD()-startdate.getdD();}
-	else if(sameYear(startdate,enddate))
-	{
-		return remainingDaysofMonth(startdate) +daysinIntervingMonth(startdate,enddate) + enddate.getdD();
-	}
-	else
-	{
-		return remainingDaysofMonth(startdate) +daysinIntervingYear(startdate,enddate)+daysinIntervingMonth(startdate,enddate)+leadingYear();
-	}
-  }
- 
+	static final int JAN = 31;
+	static final int FEB = 28;
+	static final int MAR = 31;
+	static final int APR = 30;
+	static final int MAY = 31;
+	static final int JUN = 30;
+	static final int JUL = 31;
+	static final int AUG = 31;
+	static final int SEP = 30;
+	static final int OCT = 31;
+	static final int NOV = 30;
+	static final int DEC = 31;
 
-private static boolean sameDate(MyDate startdate,MyDate enddate){
-	  return enddate.getdD()==startdate.getdD();
-  }
-  private static boolean sameMonth(MyDate startdate,MyDate enddate)
-  {
-	  return enddate.getmm()==startdate.getmm();
-  }
-  private static boolean sameYear(MyDate startdate,MyDate enddate)
-  {
-	  return enddate.getyyyy()==startdate.getyyyy();
-  }
-  private int remainingDaysofMonth(MyDate date)
-  {
-	  return MONTHS[date.getmm()-1]-date.getdD();
-  }
-private int daysinIntervingMonth(MyDate startdate,MyDate enddate)
-{
-	int total=0;
-	for(int i=startdate.getmm()+1;i<enddate.getmm();i++)
-	{
-		total+=MONTHS[i-1];
+	int[] months = { JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC };
+
+	public int getDateDifference(MyDate startDate, MyDate endDate) {
+		if (sameDate(startDate, endDate) && sameMonth(startDate, endDate) && sameYear(startDate, endDate)) {
+			return 0;
+		} else if (sameMonth(startDate, endDate) && sameYear(startDate, endDate)) {
+			return endDate.getDd() - startDate.getDd();
+		} else if (sameYear(startDate, endDate)) {
+			return remainingDaysOfMonth(startDate) + daysInIntervingMonth(startDate, endDate) + endDate.getDd();
+		} else {
+			return remainingDaysOfMonth(startDate) + remainingDaysInYear(startDate) + leadingYear(endDate)
+					+ daysInInterveningYear(startDate, endDate);
+		}
+
 	}
-	return total;
-}
-private int daysinIntervingYear(MyDate startdate, MyDate enddate) {
-	
-	
-}
+
+	private boolean sameDate(MyDate startDate, MyDate endDate) {
+		return endDate.getDd() == startDate.getDd();
+	}
+
+	private boolean sameMonth(MyDate startDate, MyDate endDate) {
+		return endDate.getMm() == startDate.getMm();
+	}
+
+	private boolean sameYear(MyDate startDate, MyDate endDate) {
+		return endDate.getYyyy() == startDate.getYyyy();
+	}
+
+	private int remainingDaysOfMonth(MyDate date) {
+		return months[date.getMm() - 1] - date.getDd();
+	}
+
+	private int daysInIntervingMonth(MyDate startDate, MyDate endDate) {
+		int totalDays = 0;
+		for (int index = startDate.getMm() + 1; index < endDate.getMm(); index++) {
+			totalDays += months[index - 1];
+		}
+		return totalDays;
+
+	}
+
+	private int daysInInterveningYear(MyDate startDate, MyDate endDate) {
+
+		int remainingdays = 0;
+
+		for (int year = startDate.getYyyy() + 1; year < endDate.getYyyy(); year++) {
+
+			remainingdays = remainingdays + 28;
+		}
+
+		return remainingdays;
+	}
+
+	private int remainingDaysInYear(MyDate startDate) {
+
+		int daysRemaining = 0;
+
+		for (int index = startDate.getMm() + 1; index <= 12; index++) {
+			daysRemaining = daysRemaining + months[index - 1];
+		}
+
+		return daysRemaining;
+
+	}
+
+	private int leadingYear(MyDate endDate) {
+
+		int daysRemaining = 0;
+
+		for (int month = 1; month < endDate.getMm(); month++) {
+			daysRemaining = daysRemaining + months[month - 1];
+		}
+
+		daysRemaining = daysRemaining + endDate.getDd();
+		return daysRemaining;
+	}
 
 }
